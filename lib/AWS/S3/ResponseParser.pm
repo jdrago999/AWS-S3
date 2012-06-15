8e9d86893d043338fd105140e05d523510d835ae
Evan Carroll <me@evancarroll.com>
2012-06-14 19:59:31 -0500

    removed BUILD, implimented triggers.

--- lib/AWS/S3/ResponseParser.pm
+++ lib/AWS/S3/ResponseParser.pm
@@ -9,7 +9,24 @@ has 'expect_nothing' => (
     is       => 'ro',
     isa      => 'Bool',
     required => 1,
-    default  => sub { 0 }
+    default  => 0,
+    trigger  => sub {
+        my ( $self, $expect_nothing, $oldValue ) = @_;
+        if ( $expect_nothing ) {
+            my $code = $self->response->code;
+            if ( $code =~ m{^2\d\d} && !$self->response->content ) {
+                return; # not sure what jdrago wanted this to do originally
+            }
+            else {
+                if ( $self->_parse_errors() ) {
+                    # die $self->friendly_error();
+                }
+                else {
+                    return;
+                }
+            }
+        }
+    }
 );
 
 has 'response' => (
@@ -47,6 +64,20 @@ has 'xpc' => (
     is       => 'ro',
     isa      => 'XML::LibXML::XPathContext',
     required => 0,
+    lazy    => 1,
+    clearer => '_clear_xpc',
+    default => sub {
+        my ( $self ) = @_;
+
+        my $src = $self->response->content;
+        return unless $src =~ m/^[[:space:]]*</s;
+        my $doc = $self->libxml->parse_string( $self->response->content );
+
+        my $xpc = XML::LibXML::XPathContext->new( $doc );
+        $xpc->registerNs( 's3', 'http://s3.amazonaws.com/doc/2006-03-01/' );
+
+        return $xpc;
+    }
 );
 
 has 'friendly_error' => (
@@ -62,28 +93,6 @@ has 'friendly_error' => (
     }
 );
 
-sub BUILD {
-    my $s = shift;
-
-    my $code = $s->response->code;
-
-    # If we got a successful response and nothing was expected, we're done:
-    if ( $s->expect_nothing ) {
-        if ( $code =~ m{^2\d\d} && !$s->response->content ) {
-            return;
-        } else {
-            if ( $s->_parse_errors() ) {
-
-                #        die $s->friendly_error();
-            } else {
-                return;
-            }    # end if()
-        }    # end if()
-    } else {
-        $s->{xpc} = $s->_xpc_of_content();
-    }    # end if()
-}    # end BUILD()
-
 sub _parse_errors {
     my ( $s ) = @_;
 
@@ -97,7 +106,7 @@ sub _parse_errors {
         return 1;
     }    # end unless()
 
-    $s->{xpc} = $s->_xpc_of_content( $src );
+    $s->_clear_xpc;
     if ( $s->xpc->findnodes( "//Error" ) ) {
         $s->error_code( $s->xpc->findvalue( "//Error/Code" ) );
         $s->error_message( $s->xpc->findvalue( "//Error/Message" ) );
@@ -106,18 +115,5 @@ sub _parse_errors {
 
     return 0;
 }    # end _parse_errors()
-
-sub _xpc_of_content {
-    my ( $s ) = @_;
-
-    my $src = $s->response->content;
-    return unless $src =~ m/^[[:space:]]*</s;
-    my $doc = $s->libxml->parse_string( $s->response->content );
-
-    my $xpc = XML::LibXML::XPathContext->new( $doc );
-    $xpc->registerNs( 's3', 'http://s3.amazonaws.com/doc/2006-03-01/' );
-
-    return $xpc;
-}    # end _xpc_of_content()
 
 __PACKAGE__->meta->make_immutable;
